03f8a0399d446c74fb9beb5c7f2a162886296900
Grzegorz Kunc <grzeg.kunc@gmail.com>
2020-05-27 17:30:25 +0200

    Redirect to song list after search is done

--- app/src/main/java/pl/musicplayer/fragments/SearchFragment.java
+++ app/src/main/java/pl/musicplayer/fragments/SearchFragment.java
@@ -1,18 +1,43 @@
 package pl.musicplayer.fragments;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import androidx.fragment.app.Fragment;
 import org.jetbrains.annotations.Nullable;
 import pl.musicplayer.R;
 
 
 public class SearchFragment extends Fragment {
+    private EditText editText;
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, null);
+        LayoutInflater lf = getActivity().getLayoutInflater();
+        view =  lf.inflate(R.layout.fragment_search, container, false);
+        editText = (EditText) view.findViewById(R.id.searchSongField);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    getActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new SongListFragment())
+                            .commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        return view;
     }
 }
