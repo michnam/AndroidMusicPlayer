46716787a1de477bb336dcee15a6c5217589b4f8
Grzegorz Kunc <grzeg.kunc@gmail.com>
2020-05-27 14:10:25 +0200

    Fix play button when changing song

--- app/src/main/java/pl/musicplayer/MainActivity.java
+++ app/src/main/java/pl/musicplayer/MainActivity.java
@@ -1,11 +1,7 @@
 package pl.musicplayer;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -16,12 +12,6 @@ import pl.musicplayer.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
 {
-    public static int songId = 1;
-    private MediaPlayer mediaPlayer;
-    private ImageButton btnPlay;
-    private boolean shouldPlay = true;
-    private TextView songTitle;
-    private TextView songAuthor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,41 +24,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(this);
-    }
-
-    public void playOrPause(View v) {
-        btnPlay = (ImageButton) findViewById(R.id.btnPlay);
-        setPlayButtonIcon(v);
-    }
-
-    private void play(View v) {
-        if (mediaPlayer == null) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.betterdays);
-        }
-        mediaPlayer.start();
-        btnPlay.setImageResource(R.drawable.pause);
-        shouldPlay = false;
-    }
-
-    private void pause(View v) {
-        if(mediaPlayer != null) {
-            mediaPlayer.pause();
-            btnPlay.setImageResource(R.drawable.play);
-            shouldPlay = true;
-        }
-    }
-
-    public void previous(View v) {
-        songId--;
-        loadFragment(new PlayerFragment());
-        setPlayButtonIcon(v);
-    }
-
-    public void next(View v) {
-        songId++;
-        System.out.println(songId);
-        loadFragment(new PlayerFragment());
-        setPlayButtonIcon(v);
     }
 
     @Override
@@ -92,33 +47,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         return loadFragment(fragment);
     }
 
-    private void setPlayButtonIcon(View v) {
-        if(shouldPlay) {
-            play(v);
-        } else {
-            pause(v);
-        }
-    }
-
     private boolean loadFragment(Fragment fragment) {
         if (fragment != null) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.fragment_container, fragment)
                     .commit();
-            return true;
-        }
-        return false;
-    }
-
-    private boolean refreshFragment(Fragment fragment) {
-        if (fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .detach(fragment)
-                    .attach(fragment)
-                    .commit();
-
             return true;
         }
         return false;
--- app/src/main/java/pl/musicplayer/fragments/PlayerFragment.java
+++ app/src/main/java/pl/musicplayer/fragments/PlayerFragment.java
@@ -1,22 +1,29 @@
 package pl.musicplayer.fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import org.jetbrains.annotations.Nullable;
 import pl.musicplayer.R;
 import pl.musicplayer.repositories.SongRepository;
 
-import static pl.musicplayer.MainActivity.songId;
-
-
 public class PlayerFragment extends Fragment {
+    private MediaPlayer mediaPlayer;
+
+    public static int songId = 1;
     private TextView songTitle;
     private TextView songAuthor;
     private SongRepository songRepository;
+    private ImageButton btnPlay;
+    private ImageButton btnNext;
+    private ImageButton btnPrevious;
+    private boolean shouldPlay = false;
+    private View view;
 
     @Nullable
     @Override
@@ -24,7 +31,30 @@ public class PlayerFragment extends Fragment {
         songRepository = new SongRepository();
 
         LayoutInflater lf = getActivity().getLayoutInflater();
-        View view =  lf.inflate(R.layout.fragment_player, container, false);
+        view =  lf.inflate(R.layout.fragment_player, container, false);
+
+        btnPlay = (ImageButton) view.findViewById(R.id.btnPlay);
+        btnPrevious = (ImageButton) view.findViewById(R.id.btnPrevious);
+        btnNext = (ImageButton) view.findViewById(R.id.btnNext);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playOrPause(view);
+            }
+        });
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previous(view);
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                next(view);
+            }
+        });
 
         songTitle = (TextView) view.findViewById(R.id.songTitle);
         songTitle.setText(songRepository.getById(songId).getTitle());
@@ -32,8 +62,78 @@ public class PlayerFragment extends Fragment {
         songAuthor = (TextView) view.findViewById(R.id.songAuthor);
         songAuthor.setText(songRepository.getById(songId).getAuthor());
 
-        System.out.println(songId);
-        System.out.println(songRepository.getById(songId).getAuthor());
+        setPlayButtonIcon(view);
         return view;
+    }
+
+    public void playOrPause(View v) {
+        if(shouldPlay) {
+            pause(v);
+        } else {
+            shouldPlay = true;
+            play(v);
+        }
+    }
+
+    public void previous(View v) {
+        songId--;
+        reloadFragment(this);
+        setPlayButtonIcon(v);
+        stop(v);
+        play(v);
+    }
+
+    public void next(View v) {
+        songId++;
+        reloadFragment(this);
+        setPlayButtonIcon(v);
+        stop(v);
+        play(v);
+    }
+
+    private void play(View v) {
+        shouldPlay = true;
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.betterdays);
+        }
+        mediaPlayer.start();
+        setPlayButtonIcon(v);
+    }
+
+    private void stop(View v) {
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    private void pause(View v) {
+        if(mediaPlayer != null) {
+            shouldPlay = false;
+            mediaPlayer.pause();
+            setPlayButtonIcon(v);
+        }
+    }
+
+    private void setPlayButtonIcon(View v) {
+        if(shouldPlay) {
+            btnPlay.setImageResource(R.drawable.pause);
+        } else {
+            btnPlay.setImageResource(R.drawable.play);
+        }
+    }
+
+    private boolean reloadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction()
+                    .detach(fragment)
+                    .attach(fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
