54036afc9e7af60e0c97ce57099bed23636eaabe
HubertFraczek <hhubcio12@gmail.com>
2020-06-04 11:23:43 +0200

    reading files from internal storage

--- app/src/main/java/pl/musicplayer/repositories/SongRepository.java
+++ app/src/main/java/pl/musicplayer/repositories/SongRepository.java
@@ -1,17 +1,9 @@
 package pl.musicplayer.repositories;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.util.Log;
-
 import pl.musicplayer.models.Song;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -19,21 +11,20 @@ import java.util.List;
  * All files should be named like band_title.mp3. If title contains whitespaces use band_long-title.mp3 convention.
  *
  * Storing mp3 files
- * All files should be stored inside /Music/AndroidMusicPlayer folder.
+ * All files should be stored inside /storage/emulated/0/Music/AndroidMusicPlayer/ folder.
  */
 public class SongRepository {
     public static List<Song> songs = getAllSongs();
     public static int currentSongId = 0;
 
     private static List<Song> getAllSongs() {
-        @SuppressLint("SdCardPath") File rootFolder = new File("/sdcard/Music/AndroidMusicPlayer");
-        File[] files = rootFolder.listFiles();
+        File file = new File("/storage/emulated/0/Music/AndroidMusicPlayer/");
+        File[] files = file.listFiles();
+        assert files != null;
         List<Song> res = new ArrayList<>();
-
         for (int i = 0; i < files.length; i++) {
             res.add(new Song(files[i].getName(), i));
         }
-
         return res;
     }
 
